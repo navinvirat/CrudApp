1d2ab5c69d0f0e4bcebfc51672fadb42f06d0324
BALASUBRAMANI <navin.balasubramani@accenture.com>
2022-05-27 11:30:20 +0530

    final changes

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -2,20 +2,102 @@ var userdb = require('../model/model');
 
 // create and save new user
 exports.create=(req,res)=>{
+// validate request
+if(!req.body){
+    res.status(400).send({message:"cannot send empty body!"});
+    return;
+}
+
+const user = new userdb({
+    name:req.body.name,
+    email:req.body.email,
+    gender:req.body.gender,
+    status:req.body.status
+})
+user 
+    .save(user)
+    .then(data=>{
+        //res.send(data)
+        res.redirect('/add-user')
+    })
+    .catch(err=>{
+        res.status(500).send({message:err.message||"cannot send empty body!"});
+    });
 
 }
 
 // retrive and return all users
 exports.find=(req,res)=>{
+
+    if(req.query.id){
+        const id= req.query.id;
+
+        userdb.findById(id)
+        .then(data=>{
+            if(!data){
+                res.status(404).send({message:`Cannot find user with ${id},may be id not found!`});
+            }
+            else{
+                res.send(data);
+            }
+        })
+        .catch(err=>{
+            res.status(500).send({message:err.message||"ERROR occured while retrive specific userID"});
+        })
+
+
+    }
+    else{
+        userdb.find()
+    .then(user=>{
+        res.send(user)
+    })
+    .catch(err=>{
+        res.status(500).send({message:err.message||"ERROR occured while retrive"});
+    })
+
+    }
+    
     
 }
 
 // update single user using id
 exports.update=(req,res)=>{
+    if(!req.body){
+        res.status(400).send({message:"update req cannot send empty body!"});
+        return;
+    }
+    const id= req.params.id;
+    userdb.findByIdAndUpdate(id,req.body, {useFindAndModify:false})
+    .then(data=>{
+        if(!data){
+            res.status(404).send({message:`Cannot update user with ${id},may be id not found!`});
+        }
+        else{
+            res.send(data);
+        }
+    })
+    .catch(err=>{
+        res.status(500).send({message:err.message||"ERROR occured while Update"});
+    })
     
 }
 
 // delete single user using id
 exports.delete=(req,res)=>{
-    
+
+    const id= req.params.id;
+
+    userdb.findByIdAndDelete(id) 
+    .then(data=>{
+        if(!data){
+            res.status(404).send({message:`Cannot delete user with ${id},may be id not found!`});
+        }
+        else{
+            res.send({message:"User data deleted successfully!"});
+        }
+    })  
+    .catch(err=>{
+        res.status(500).send({message:err.message||"ERROR occured while Delete!"});
+    })
 }
--- server/services/render.js
+++ server/services/render.js
@@ -1,9 +1,31 @@
+const axios= require('axios');
+const { response } = require('express');
+
 exports.home=(req,res)=>{
-    res.render('index');
+
+    // Make get request to the /api/users/
+    axios.get('http://localhost:3000/api/users')
+    .then((response)=>{
+        console.log(response.data);
+        res.render('index',{user:response.data});
+    })
+    .catch(err=>{
+        res.send(err);
+    }) 
+    
+    
 }
 exports.add_user=(req,res)=>{
     res.render('add_user');
 }
 exports.update_user=(req,res)=>{
-    res.render('update_user');
+    axios.get('http://localhost:3000/api/users',{params:{id:req.query.id}})
+    .then(function(userdata){
+        res.render('update_user',{user:userdata.data});
+        // console.log("Navin",{user:userdata.data})
+    })
+    .catch(err=>{
+        res.send(err);
+    })
+    // res.render('update_user');
 }
